4674ab4eb8438d72532e68bef804505f79413438
salmon <duckduckcute74@gmail.com>
2024-10-06 16:29:40 +0800

    moving cube
    a cube can move

--- Assets/move.cs
+++ Assets/move.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class move : MonoBehaviour
 {
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = -20.0f;
+    public float gravity = 20.0f;
 
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
